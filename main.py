1430e505f7e4e87125f1a48dcf38bf529744fde8
Smart developer <developer@yandex.ru>
2024-12-01 20:53:02 +0300

    go

--- main.py
+++ main.py
@@ -8,30 +8,12 @@ from PyQt6.QtWidgets import QDialog
 from PyQt6.QtGui import QPainter, QColor
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(990, 826)
-        self.create = QtWidgets.QPushButton(parent=Dialog)
-        self.create.setGeometry(QtCore.QRect(410, 740, 171, 61))
-        self.create.setObjectName("create")
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.create.setText(_translate("Dialog", "Создать"))
-
-
-class MyWidget(QMainWindow, Ui_Dialog):
+class MyWidget(QMainWindow, QDialog):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.do_paint = False
         self.circles = []
-        self.colors = []
         self.create.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -47,12 +29,11 @@ class MyWidget(QMainWindow, Ui_Dialog):
         self.update()
 
     def draw_flag(self, qp):
-        color = (randint(0, 255), randint(0, 255), randint(0, 255))
-        self.colors.append(color)
+        qp.setBrush(QColor(255, 255, 0))
         self.circles.append((randint(0, 800),randint(0, 800), randint(0, 400)))
-        for i in range(len(self.circles)):
-            qp.setBrush(QColor(*self.colors[i]))
-            qp.drawEllipse(self.circles[i][0], self.circles[i][1], self.circles[i][2], self.circles[i][2])
+        for i in self.circles:
+            qp.drawEllipse(i[0], i[1], i[2], i[2])
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
