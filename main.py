fdecacc035b1b836acc1000612d025f89c9eb6ec
Smart developer <developer@yandex.ru>
2024-12-01 19:56:21 +0300

    go

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class MyWidget(QMainWindow, QDialog):
 
     def draw_flag(self, qp):
         qp.setBrush(QColor(255, 255, 0))
-        self.circles.append((randint(0, 800),randint(0, 800), randint(0, 800)))
+        self.circles.append((randint(0, 800),randint(0, 800), randint(0, 400)))
         for i in self.circles:
             qp.drawEllipse(i[0], i[1], i[2], i[2])
 
