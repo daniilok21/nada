fa4c979fb1a1ddaefc740889494988b2c13b35af
Smart developer <developer@yandex.ru>
2024-12-01 20:45:59 +0300

    go

--- main.py
+++ main.py
@@ -8,27 +8,10 @@ from PyQt6.QtWidgets import QDialog
 from PyQt6.QtGui import QPainter, QColor
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(990, 826)
-        self.create = QtWidgets.QPushButton(parent=Dialog)
-        self.create.setGeometry(QtCore.QRect(410, 740, 171, 61))
-        self.create.setObjectName("create")
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.create.setText(_translate("Dialog", "Создать"))
-
-
-class MyWidget(QMainWindow, Ui_Dialog):
+class MyWidget(QMainWindow, QDialog):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.do_paint = False
         self.circles = []
         self.create.clicked.connect(self.paint)
