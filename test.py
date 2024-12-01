ddddb6588bcda98c68d7801bb6f07498dcc72f6c
Smart developer <developer@yandex.ru>
2024-12-01 20:56:26 +0300

    go123

---
+++ test.py
@@ -0,0 +1,42 @@
+import sys
+
+from random import randint
+from PyQt6 import uic  # Импортируем uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QDialog
+from PyQt6.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow, QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.circles = []
+        self.create.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        self.circles.append((randint(0, 800),randint(0, 800), randint(0, 400)))
+        for i in self.circles:
+            qp.drawEllipse(i[0], i[1], i[2], i[2])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
